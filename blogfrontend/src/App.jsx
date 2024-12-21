81dda672b7ed865c914fab9d984a338b9d5a9207
Preethuakhilraj <46960642+Preethuakhilraj@users.noreply.github.com>
2024-12-21 18:32:42 +0530

    Update App.jsx

--- blogfrontend/src/App.jsx
+++ blogfrontend/src/App.jsx
@@ -6,9 +6,7 @@ import Blogs from './Pages/Blogs';
 import Addblog from './Pages/Addblog';
 import Main from './Pages/Main';
 import Privateroutes from './Pages/Privateroutes'; // Adjust the path as per your folder structure
-import BlogDetail from './Pages/BlogDetail.jsx';
-
-
+import BlogDetail from './Pages/Blogdetail';
 function App() {
   return (
     <Router>
